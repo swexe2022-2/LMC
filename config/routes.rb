501dc470dc87a43ef400791d96b62b2bff3c56c4
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-11-09 07:28:27 +0000

    routes.rb修正
    routes.rbファイル修正と、recipes/index.html.erbが必要だったため新規作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create, :destroy]
   resources :recipes
+  get 'recipes/{:id}', to: 'recipes#show'
+  post 'recipes/{:id}/edit', to: 'recipes#edit'
+  get 'users/{:id}', to: 'users#show'
+  post 'users/{:id}/edit', to: 'users#edit'
   root 'recipes#index'
-  post 'top#login'
+  post 'top/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
