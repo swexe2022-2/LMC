9c59ae4274345bd7de5706aa5d074e0a94f4c9e2
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-11-23 07:19:15 +0000

    不要なコード削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :destroy, :edit, :update, :show]
   resources :recipes
-  # get 'recipes/{:id}', to: 'recipes#show'
-  # post 'recipes/{:id}/edit', to: 'recipes#edit'
-  # get 'users/{:id}', to: 'users#show'
-  # post 'users/{:id}/edit', to: 'users#edit'
-  patch "users/:id/update", to: "users#update"
   root 'recipes#index'
   get 'top/main'
   post 'top/login'
