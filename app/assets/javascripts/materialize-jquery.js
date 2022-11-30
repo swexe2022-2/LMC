8b2095067306875ec0669b903b8c82b279a27e01
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2022-11-30 09:10:53 +0000

    ナビゲーションバー

---
+++ app/assets/javascripts/materialize-jquery.js
@@ -0,0 +1,15 @@
+/*global $*/
+$(document).on('turbolinks:load', function () {
+  // 各項目を初期化
+  $('.sidenav').sidenav();
+  $('.parallax').parallax();
+  $('.dropdown-trigger').dropdown();
+  $('.modal').modal();
+});
+
+// sidenavだけは、進む/戻るで動かなくなるので都度destroyが必要
+$(document).on("turbolinks:before-cache", function () {
+  $('.sidenav').sidenav('destroy');
+  $('.dropdown-trigger').dropdown('destroy');
+  $('.modal').modal('destroy');
+});
