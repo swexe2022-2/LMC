d68cdbca563e11f272a0d1478bfad4a0fe431537
2011140002 <2011140002p@ed.fuk.kindai.ac.jp>
2023-01-17 18:42:12 +0000

    ユーザーコントローラーのupdateを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,33 +4,48 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
-    @recipes = Recipe.where( user_id: params[:id])
+    @recipes = Recipe.where( user_id: current_user.id)
   end
   
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
   
   end
   
   def update
-    if params[:user][:name] == nil or params[:user][:mail] == nil
-      flash[:notice] = 'アカウント情報の更新に失敗しました'
-      render 'edit'
-    else
-      @user = User.find(params[:id])
-      logger.debug(params[:id])
+    
+      @user = current_user
+      logger.debug(params[:user][:name])
+      logger.debug(params[:user][:mail])
+      logger.debug(params[:user][:password])
       name = params[:user][:name]
       mail = params[:user][:mail]
-      if @user.update(name: name, mail: mail)
-        redirect_to user_path(params[:id])
+      password = params[:user][:password]
+      password_confirmation=params[:user][:password_confirmation]
+      
+      if password == nil or password_confirmation == nil
+        if @user.update(name: name, mail: mail)
+          flash[:notice] = 'アカウント情報を変更しました'
+          redirect_to user_path(current_user.id)
+        else
+          flash[:notice] = 'アカウント情報の更新に失敗しました'
+          redirect_to edit_user_path("#{current_user.id}")
+        end
       else
-        flash[:notice] = 'アカウント情報の更新に失敗しました'
-        logger.debug(params[:user][:name])
-        logger.debug(params[:user][:mail])
-        render 'edit'
+        if password != password_confirmation
+          flash[:notice] = 'パスワードが一致していません'
+          redirect_to edit_user_path("#{current_user.id}")
+        elsif @user.update(name: name, mail: mail,
+          password: password, password_confirmation: password_confirmation)
+          flash[:notice] = 'アカウント情報を変更しました'
+          redirect_to user_path(current_user.id)
+        else
+          flash[:notice] = 'アカウント情報の更新に失敗しました'
+          redirect_to edit_user_path("#{current_user.id}")
+        end
+        
       end
-    end
+    
       
   end
   
